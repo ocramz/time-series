17098279b05cba0250d60353f9cdaea58bb135cf
ocramz <zocca.marco@gmail.com>
2017-06-28 10:06:16 +0200

    added conduit-extra and exceptions

--- app/forex/Main.hs
+++ app/forex/Main.hs
@@ -5,6 +5,9 @@ import Lib
 -- import Chart.Unit
 -- import Chart.Types
 
+import Control.Monad.Catch
+import Data.Conduit
+import Data.Text
 import qualified Data.Conduit.Attoparsec as CA
 
 
@@ -12,3 +15,6 @@ import qualified Data.Conduit.Attoparsec as CA
 main = putStrLn "hello!"
 
 
+
+asdf :: MonadThrow m => ConduitM Text o m (FxDataSet Double)
+asdf = CA.sinkParser parseFxDataset
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,7 @@
 module Lib (module X) where
 
 import Lib.Parsers.Forex as X
-
+import Data.TimeSeries.Forex.Types as X
 
 
 
