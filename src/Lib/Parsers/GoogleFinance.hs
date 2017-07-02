e65ce4276414f8f19c96a66d1b4777bee6c6e0d6
ocramz <zocca.marco@gmail.com>
2017-07-02 23:33:19 +0200

    added format info

--- src/Lib/Parsers/GoogleFinance.hs
+++ src/Lib/Parsers/GoogleFinance.hs
@@ -9,7 +9,17 @@ import Data.Text
 import Data.Time (Day, TimeOfDay)
 
 
--- example data
+
+
+-- example qery and data
+
+-- The URL format is: https://www.google.com/finance/getprices?i=[PERIOD]&p=[DAYS]d&f=d,o,h,l,c,v&df=cpct&q=[TICKER]
+
+-- Example: https://www.google.com/finance/getprices?i=60&p=10d&f=d,o,h,l,c,v&df=cpct&q=IBM
+
+-- [PERIOD]: Interval or frequency in seconds
+-- [DAYS]: The historical data period, where "10d" means that we need historical stock prices data for the past 10 days.
+-- [TICKER]: This is the ticker symbol of the stock
 
 -- EXCHANGE%3DNYSE
 -- MARKET_OPEN_MINUTE=570
